9412b6945226a5a24fdd811f6f22b2ee9a42b78f
mpb85 <malutenkoroman@gmail.com>
2026-06-27 23:51:13 +0300

    feat: add burger menu logic and interactivity

--- shelter/pages/main/index.js
+++ shelter/pages/main/index.js
@@ -1,1 +1,42 @@
-console.log("")
+/*----------------------------- Humburger menu ------------------------------------*/
+const hamburgerMenu = document.querySelector('.nav__hamburger');
+const headerMenu = document.querySelector('.header__menu');
+const menuList = document.querySelector('.menu__list');
+const body = document.querySelector('body');
+
+ hamburgerMenu.addEventListener('click', ()=>{
+    openBurger();    
+});
+
+menuList.addEventListener('click', (e)=>{
+    const burgerBtn = e.target.closest('.menu__item');
+    if(!burgerBtn) return;
+    closeBurger();
+})
+
+document.addEventListener('click', (e)=>{
+    if(!hamburgerMenu.contains(e.target) && !headerMenu.contains(e.target)){
+        closeBurger();
+    }
+})
+
+function openBurger() {
+    hamburgerMenu.classList.toggle('active');
+    headerMenu.classList.toggle('show');
+    document.body.classList.toggle('_lock');
+    document.addEventListener('click', closeBurgerOnClickOutside);
+}
+function closeBurger() {
+    hamburgerMenu.classList.remove('active');
+    headerMenu.classList.remove('show');
+    document.body.classList.remove('_lock');
+    document.removeEventListener('click', closeBurgerOnClickOutside)
+}
+
+function closeBurgerOnClickOutside(e) {
+    if (!hamburgerMenu.contains(e.target) && !headerMenu.contains(e.target)) {
+        closeBurger();
+    }
+}
+
+
